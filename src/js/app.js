e44e9a37069eee449884df7cc0309f18479716d1
amir <amir.hz.09921103757@gmail.com>
2022-06-28 15:21:45 +0430

    Update Up to suggestion-slider-best

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,25 @@ var btnNext = document.querySelector(".btn-next");
 var number = document.getElementsByClassName("number");
 var i = 0;
 var n;
+// header shadow
+let header = document.getElementById("header");
+document.onscroll = function () {
+  header.style.boxShadow = "0 0 2px 2px #eee";
+};
+// popup message
+function popUP() {
+  let popUpimage = document.getElementById("popup-image");
+  let closePopup = document.getElementById("btn-close-popup");
+  popUpimage.style.right = '0px';
+  closePopup.addEventListener('click',(e)=>{
+    e.preventDefault();
+    popUpimage.style.right = '-500px';
+  });
+}
+setTimeout(() => {
+  popUP();
+}, 20000);
+// start slider
 function backSlidediNone() {
   for (n = 0; n < slider.length; n++) {
     slider[n].style.display = "none";
@@ -27,7 +46,7 @@ btnNext.addEventListener("click", function () {
 btnBack.addEventListener("click", function () {
   i--;
   if (i < 0) {
-    i = slider.length -1;
+    i = slider.length - 1;
   }
   backSlidediNone();
   noActiveNum();
@@ -35,16 +54,13 @@ btnBack.addEventListener("click", function () {
   number[i].classList.add("active-number");
 });
 setInterval(() => {
-    i++;
-    if (i > slider.length - 1) {
-      i = 0;
-    }
-    backSlidediNone();
-    noActiveNum();
-    slider[i].style.display = "block";
-    number[i].classList.add("active-number");
+  i++;
+  if (i > slider.length - 1) {
+    i = 0;
+  }
+  backSlidediNone();
+  noActiveNum();
+  slider[i].style.display = "block";
+  number[i].classList.add("active-number");
 }, 5000);
-let header = document.getElementById("header");
-document.onscroll = function() {
-      header.style.boxShadow = '0 0 2px 2px #eee';
-}
+// end slider
